6e0a11f4ec12ac4f7e1f8bbbe12eed4d02cfce95
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-10-13 18:05:42 +0300

    add object destroy

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+	private int _collisionCounter;
+
+	private void Start()
+	{
+		_collisionCounter = 0;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!other.CompareTag("Player")) return;
+		_collisionCounter++;
+		if (_collisionCounter < 2) return;
+		Destroy(gameObject);
+		Destroy(other.gameObject);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (_collisionCounter > 0) _collisionCounter--;
+	}
+}
