a18e32871ad4e0a0ff766e8f858411024e34c6dc
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-10-15 19:05:42 +0300

    add explosion to asteroid

--- Assets/Scripts/DestroyDangerousByContact.cs
+++ Assets/Scripts/DestroyDangerousByContact.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class DestroyDangerousByContact : MonoBehaviour {
+public class DestroyDangerousByContact : MonoBehaviour
+{
 
+	public GameObject Explosion;
+	
 	private void OnTriggerEnter(Collider other)
 	{
 		if (!other.CompareTag("Dangerous")) return;
 		Destroy(gameObject);
 		Destroy(other.gameObject);
+		Instantiate(Explosion, other.transform.position, other.transform.rotation);
 	}
 }
